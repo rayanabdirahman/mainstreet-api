72fdf3881e6b2874cbed2d315b84c88ea3ab33e7
rayanabdirahman <rayanmabdirahman@gmail.com>
2021-01-31 14:57:54 +0000

    add inversify config

---
+++ src/inversify.config.ts
@@ -0,0 +1,14 @@
+import 'reflect-metadata'
+import { Container } from 'inversify'
+import TYPES from './types'
+import { RegistrableController } from './api/registrable.controller'
+
+const container = new Container()
+
+// controllers
+
+// services
+
+// repository
+
+export default container
